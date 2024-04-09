04fade90466fbd48059cd96b167762dbe8153c90
Tumaini <tumainikebaya.234@gmail.com>
2024-04-09 22:07:08 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+
+  function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+
+/*
+function superBowlWin(array){
+
+    for(const object of array){
+        if (object.result === "W"){
+            console.log(object.year);
+        } else {
+            return undefined;
+        }
+    }
+}
+superBowlWin(record)
+*/
